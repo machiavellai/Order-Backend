6c0ed471f6e3cc8628fca586a87c28d8bade7db7
Victor oghene <93010377+machiavellai@users.noreply.github.com>
2024-10-10 15:27:23 +0100

    Otp key (#2)
    * Redis client set-up
    
    * ✅ movement⚡️
    
    ---------
    
    Co-authored-by: machiavellai <oghenevicor54@gmail.com>

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const StartServer = async () => {
     await DBConnection();
 
     await App(app);
-
+ 
     app.listen(8000, () => {
         console.log('listening on port 8000');
 
--- services/Database.ts
+++ services/Database.ts
@@ -1,11 +1,25 @@
 import mongoose from 'mongoose';
-import { MONGO_URI } from '../config';
+import { MONGO_URI, REDIS_URL } from '../config';
+import { createClient } from 'redis';
+
+// Redis client setup
+const redisClient = createClient({
+    url: REDIS_URL || 'redis://localhost:6379'  // Redis URL (use default or from config)
+});
+
 
 export default async () => {
-    await mongoose.connect(MONGO_URI)
-        .then(result => {
-            console.log('connected to DB');
+    try {
+        await mongoose.connect(MONGO_URI)
+        console.log('connected to DB');
 
-        }).catch(err => console.log('error' + err))
+        //redis connection
+        redisClient.on('error', (err) => console.log('Redis Client Error', err));
+        await redisClient.connect();
+        console.log('Connected to Redis');
+    } catch (err) {
+        console.log('error' + err)
+    }
 
 }
+export { redisClient }; 
