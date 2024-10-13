304b26f91b3a7b5daa82ae6fa5333f1831a8c2b5
machiavellai <oghenevicor54@gmail.com>
2024-10-13 17:32:47 +0100

    ju

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.REDIS_URL = exports.APP_SECRET = exports.MONGO_URI = void 0;
+exports.PORT = exports.REDIS_URL = exports.APP_SECRET = exports.MONGO_URI = void 0;
 exports.MONGO_URI = 'mongodb+srv://oghenevictor54p:x1FvXwIK87MB0ZFH@cluster0.j925o.mongodb.net/';
 exports.APP_SECRET = "Our_App_Secret";
 exports.REDIS_URL = "redis://default:ipQfaJKsxQKIVp5AHIYWOvn1caFpnj7U@redis-14669.c93.us-east-1-3.ec2.redns.redis-cloud.com:14669";
+exports.PORT = process.env.PORT || 8000;
 //# sourceMappingURL=index.js.map
